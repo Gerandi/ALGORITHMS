059e1647adcfdd0c0ed428403de9f5a17253d38c
geri <gerandi1997@gmail.com>
2023-02-07 12:02:19 +0100

    Fundamentals To Do 1

--- todo1.js
+++ todo1.js
@@ -162,7 +162,7 @@ function flexibleCountdown(lowNum, highNum, mult) {
 
 flexibleCountdown(2, 9, 3);
 
-// The Final Countdown
+// The Final Countdown.
 // This is based on "Flexible Countdown". The parameter names are not as helpful, but the problem is essentially identical; don't be thrown off! Given 4 parameters (param1,param2,param3,param4), print the multiples of param1, starting at param2 and extending to param3. One exception: if a multiple is equal to param4, then skip (don't print) it. Do this using a WHILE. Given (3,5,17,9), print 6,12,15 (which are all of the multiples of 3 between 5 and 17, and excluding the value 9).
 
 function finalCountdown(param1, param2, param3, param4) {
